a09e4f2d2f07777cc7e7ba653a5ffe5f525d3db6
Aakash Jangid <51054507+aakashjangidme@users.noreply.github.com>
2023-01-30 19:38:46 +0530

    initial commit

---
+++ src/main/java/me/ajangid/playgroundxspringbootboilerplate/PlaygroundXSpringBootBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package me.ajangid.playgroundxspringbootboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundXSpringBootBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundXSpringBootBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ajangid/playgroundxspringbootboilerplate/PlaygroundXSpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ajangid.playgroundxspringbootboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundXSpringBootBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
